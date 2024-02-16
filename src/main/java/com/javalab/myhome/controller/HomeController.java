23a9544958b9f3683667d4dc94af22b7cb2a03ec
seungHee-cat <kye1281@naver.com>
2024-02-17 00:13:26 +0900

    layout 구성

---
+++ src/main/java/com/javalab/myhome/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.javalab.myhome.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping
+    public String index(){
+        return "index";
+    }
+}
